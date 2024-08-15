5a31eaa8e5a9d4e2e0f9f82993f418e971cdc4de
J-CamiloG <jc6381878@gmail.com>
2024-08-14 20:32:13 -0500

    Set up the basic structure of the project and initialized the repository.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Views/**/*.cshtml',
+    './wwwroot/js/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
